72683cc6253365ea84060f4054c147ceade7d7e6
LarsWinters <lucas.winkler1998@googlemail.com>
2021-12-22 18:45:16 +0100

    test kathrin

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("Test")
     print("test123")
+    print("test Kathrin")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
