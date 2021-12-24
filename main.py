35bc5dc257463370ebccd8b63a6882d40e488e5c
LarsWinters <lucas.winkler1998@googlemail.com>
2021-12-24 12:20:34 +0100

    train_files shape function

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 #here are all imports necessary for the CNN
 import tensorflow as tf
-import zipfile as zp
 import os
 import glob
+from PIL import Image
+import pandas as pd
 
 #define paths --> data should be in github repo --> need to be changed
 train_path = 'C:/Users/Lucas/Desktop/Data_BIS_CNN/'
@@ -14,15 +15,23 @@ pred_path = 'C:/Users/Lucas/Desktop/Data_BIS_CNN/'
 def train_folders():
     for folder in os.listdir(train_path + 'seg_train'):
         files = glob.glob(pathname=train_path + "seg_train//" + folder + '/*.jpg')
-        print(f'({folder}) folder has: {len(files)}')
+        #print(f'({folder}) folder has: {len(files)}')
+    return
+
+def train_files():
+    train_img_size = []
+    for folder in os.listdir(train_path + 'seg_train'):
+        files = glob.glob(pathname = train_path + "seg_train//" + folder + '/*.jpg')
+        for file in files:
+            im = Image.open(file)
+            train_img_size.append(im.size)
+    pd.Series(train_img_size).value_counts()
     return
 #model architecture
+
 def main():
-    hello = tf.constant("hello, Tensorflow!")
-    tf.print(hello)
-    print(hello)
-    print("Tensorflow Version: {}".format(tf.__version__))
     train_folders()
+    train_files()
 
 if __name__ == "__main__":
     main()
